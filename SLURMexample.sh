15bf76223424c6164d49bdc6f71c98bcc547d022
ga45yax <ga45yax@mytum.de>
2019-12-09 14:03:59 +0100

    1

--- SLURMexample.sh
+++ SLURMexample.sh
@@ -21,12 +21,13 @@ export I_MPI_PIN=1
 MATRIX_PATH=../chameleon-apps/applications/matrix_example
 
 echo run
-mpirun -np 1 ./interference > out
-time mpirun -np 1 ./$MATRIX_PATH/main 300 > mat
+mpirun -n 1 ./interference > out
+
 #
 # run program
 #
 
+#time mpirun -np 1 ./$MATRIX_PATH/main 300 > mat
 
 echo end
 exit 0
