f5f671225ff24ad078f0239c472b00619a488850
ga45yax <ga45yax@mytum.de>
2019-12-10 12:26:35 +0100

    8

--- interference_main.cpp
+++ interference_main.cpp
@@ -29,8 +29,8 @@ int main(int argc, char **argv)
 	}
 
 
-	float targetFractionMin = .5;//atof(argv[1]);
-	float targetFractionMax = .8;//atof(argv[1]);
+	float targetFractionMin = .01;//atof(argv[1]);
+	float targetFractionMax = .01;//atof(argv[1]);
 
 	if (targetFractionMin > 1) targetFractionMin = 1 / targetFractionMin;
 	if (targetFractionMax > 1) targetFractionMax = 1 / targetFractionMax;
