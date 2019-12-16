bbde9a957969d729cfbf9490cf4e8641528f0f22
ga45yax <ga45yax@mytum.de>
2019-12-16 13:16:15 +0100

    e

---
+++ enigma.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#########################
+#
+#SBATCH --job-name=scaling-test
+#SBATCH -o ./enigma.out
+#SBATCH -D ./
+#SBATCH --get-user-env
+#SBATCH --clusters=mpp2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --time=0-00:02:00
+#
+
+echo initiate
+module load slurm_setup
+export OMP_NUM_THREADS=28
+
+#export OMP_PROC_BIND=close
+#export OMP_PLACES=cores
+
+export I_MPI_PIN_DOMAIN=auto
+export I_MPI_PIN=1
+
+#export VT_PCTRACE=1
+
+
+MATRIX_PATH=../chameleon-apps/applications/matrix_example
+
+echo run
+mpiexec -n 1 ./interference > out &
+
+#
+# run program
+#
+
+mpiexec -n 1 ./$MATRIX_PATH/main 500 > mat &
+wait
+
+echo end
+exit 0
