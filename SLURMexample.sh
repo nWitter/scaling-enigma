bdac0b567599d2faafce340e571dc728875a07bc
ga45yax <ga45yax@mytum.de>
2019-12-16 13:02:35 +0100

    5

--- SLURMexample.sh
+++ SLURMexample.sh
@@ -2,7 +2,12 @@
 #########################
 #
 #SBATCH --job-name=scaling-test
-#
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./
+#SBATCH --get-user-env
+#SBATCH --clusters=mpp2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
 #SBATCH --nodes=2
 #SBATCH --time=0-00:03:00
 #
@@ -16,7 +21,7 @@
 echo initiate things
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
-export OMP_NUM_THREADS=20
+#export OMP_NUM_THREADS=20
 
 export I_MPI_PIN_DOMAIN=auto
 export I_MPI_PIN=1
@@ -27,13 +32,14 @@ export VT_PCTRACE=1
 MATRIX_PATH=../chameleon-apps/applications/matrix_example
 
 echo run
-mpiexec -n 1 ./interference > out
+mpiexec -n $SLURM_NTASKS ./interference > out &
 
 #
 # run program
 #
 
-mpiexec -n 2 ./$MATRIX_PATH/main 500 20 20 > mat
+mpiexec -n $SLURM_NTASKS ./$MATRIX_PATH/main 500 20 20 > mat &
+wait
 
 echo end
 exit 0
