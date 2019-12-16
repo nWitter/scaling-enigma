3aba9d7dd48bdc49449d36a8c1398a8c14f3219a
ga45yax <ga45yax@mytum.de>
2019-12-16 13:06:23 +0100

    f

--- SLURMexample.sh
+++ SLURMexample.sh
@@ -8,26 +8,21 @@
 #SBATCH --clusters=mpp2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=28
-#SBATCH --nodes=2
 #SBATCH --time=0-00:03:00
 #
 
+echo initiate
+module load slurm_setup
+export OMP_NUM_THREADS=28
 
-#
-#SBATCH --cpus-per-task=4
-#
-#
-
-echo initiate things
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
-#export OMP_NUM_THREADS=20
 
 export I_MPI_PIN_DOMAIN=auto
 export I_MPI_PIN=1
 
+#export VT_PCTRACE=1
 
-export VT_PCTRACE=1
 
 MATRIX_PATH=../chameleon-apps/applications/matrix_example
 
