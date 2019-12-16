09cb46c0bda4bff6ecca5790e53270d408a21eb8
ga45yax <ga45yax@mytum.de>
2019-12-16 13:30:48 +0100

    2

--- enigma.sh
+++ enigma.sh
@@ -12,17 +12,19 @@
 #SBATCH --time=0-00:02:00
 #
 
+#
+
 echo initiate
-module load slurm_setup
+#module load slurm_setup
 export OMP_NUM_THREADS=28
 
-#export OMP_PROC_BIND=close
-#export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
 
 export I_MPI_PIN_DOMAIN=auto
 export I_MPI_PIN=1
 
-#export VT_PCTRACE=1
+export VT_PCTRACE=1
 
 
 
