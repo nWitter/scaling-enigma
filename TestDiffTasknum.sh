55fc5c9e320334cf3c50ffc352c2e0a9e270784e
ga45yax <ga45yax@mytum.de>
2020-01-04 17:39:31 +0100

    1

--- TestDiffTasknum.sh
+++ TestDiffTasknum.sh
@@ -18,7 +18,7 @@ salloc --partition=mpp2_inter --ntasks=1 --cpus-per-task=1 --time=2
 echo "allocated 1 core"
 export OMP_NUM_THREADS=1
 mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH 200 > mat1.out
-
+exit 0
 
 #2
 echo "starting 2 cores"
@@ -26,3 +26,7 @@ salloc --partition=mpp2_inter --ntasks=2 --cpus-per-task=1 --time=2
 echo "allocated 2 cores"
 export OMP_NUM_THREADS=2
 mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH 200 > mat2.out
+exit 0
+
+
+exit 0
