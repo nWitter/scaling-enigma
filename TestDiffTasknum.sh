19eb521564b5e566d4f128d6676a8e085a275a4e
ga45yax <ga45yax@mytum.de>
2020-01-04 18:18:23 +0100

    1

--- TestDiffTasknum.sh
+++ TestDiffTasknum.sh
@@ -7,26 +7,20 @@ export I_MPI_PIN_DOMAIN=auto
 export I_MPI_PIN=1
 #export VT_PCTRACE=1
 
-MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
-MATRIX_SIZE=600
+#MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
+#MATRIX_SIZE=600
 
-chmod +x startInterference.sh
+
+echo "n=1 t=7"
+sbatch enigma.sh --ntasks=1 --cpus-per-task=7 400
+
 
 #1
-echo "starting 1 core"
-salloc --partition=mpp2_inter --ntasks=1 --cpus-per-task=1 --time=2
-echo "allocated 1 core"
-export OMP_NUM_THREADS=1
-mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH 200 > mat1.out
-exit 0
-
-#2
-echo "starting 2 cores"
-salloc --partition=mpp2_inter --ntasks=2 --cpus-per-task=1 --time=2
-echo "allocated 2 cores"
-export OMP_NUM_THREADS=2
-mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH 200 > mat2.out
-exit 0
-
+#echo "starting 1 core"
+#salloc --partition=mpp2_inter --ntasks=1 --cpus-per-task=1 --time=2
+#echo "allocated 1 core"
+#export OMP_NUM_THREADS=1
+#mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH 200 > mat1.out
+#exit 0
 
 exit 0
--- enigma.sh
+++ enigma.sh
@@ -12,17 +12,16 @@
 #
 
 module load slurm_setup
-export OMP_NUM_THREADS=28
 export I_MPI_PIN_DOMAIN=auto
 export I_MPI_PIN=1
 #export VT_PCTRACE=1
 
 MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
-MATRIX_SIZE=600
+MATRIX_SIZE=@0
 
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 echo "init"
-chmod +x startInterference.sh
 mpiexec -n $SLURM_NTASKS ./startInterference.sh $MATRIX_PATH $MATRIX_SIZE
 
 exit 0
