d39902b34a0f440e84cd8176b50f7506e93c891f
ga45yax <ga45yax@mytum.de>
2020-01-06 20:40:51 +0100

    2

--- MPI_Manager.cpp
+++ MPI_Manager.cpp
@@ -23,6 +23,7 @@ int main(int argc, char **argv)
 	char inmsg, outmsg = 'x';
 	MPI_Status Stat;
 	char intrfStatus = '0';
+	int tst = 0;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
@@ -31,24 +32,25 @@ int main(int argc, char **argv)
 
 	//while (true) {
 		if (rank == 0) {
+			tst++;
+			printf("sth\n");
+			MPI_Recv(&inmsg, 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD, &Stat);
+			printf("%c \n", inmsg);
+			MPI_Recv(&inmsg, 1, MPI_CHAR, 2, tag, MPI_COMM_WORLD, &Stat);
+			printf("%c \n", inmsg);
 			
 			for(int i = 1;i<numtasks;i++){
 				dest = i;
 				outmsg = '1';
 				MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
 			}
-			for(int i = 1;i<numtasks;i++){
-				source = i;
-				MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-			printf("%c \n", inmsg);
-			}
 
 		} else if (rank != 0) {
+			//tst
+			MPI_Send(&outmsg, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD);
+			
 			source = 0;
 			MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-			
-			printf("%c \n", inmsg);
-			MPI_Send(&outmsg, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD);
 		}
 
 
@@ -57,6 +59,7 @@ int main(int argc, char **argv)
 
 	printf("done .\n");
    MPI_Get_count(&Stat, MPI_CHAR, &count);
+   printf("Num: %d", tst);
    printf("Task %d: Received %d char(s) from task %d with tag %d \n",
           rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
 
