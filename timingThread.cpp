881ef28f527ef6b66a0b70f98318af54192ee6e5
ga45yax <ga45yax@mytum.de>
2020-01-08 18:58:51 +0100

    3

--- timingThread.cpp
+++ timingThread.cpp
@@ -37,9 +37,9 @@ int main(int argc, char **argv) {
 
     // Do something for a long time.
 	printf("gogo");
-	for(int a = 2;a<11000;a*=2){
+	for(int a = 2;a<1100;a*=2){
 		//theory: maximize sceduled time-working????
-		std::this_thread::sleep_for(microseconds(1));
+		std::this_thread::sleep_for(microseconds(1000));
 		printf("zzz");
 		
 		
