05e0e8bdcf32b19f68d416e1753326e549c100cd
ga45yax <ga45yax@mytum.de>
2020-01-09 18:02:21 +0100

    )

--- timingThread.cpp
+++ timingThread.cpp
@@ -17,10 +17,10 @@ extern "C" void interrupt_processing(int s) {
 }
 
 int tNow(Clock::time_point tZero){
-	auto t1 = Clock::now();
-	microseconds d = microseconds(t1 - tZero);
+	auto t1 = system_clock::time_point(milliseconds(Clock::now()));
+	//microseconds d = t1 - tZero;
 	//microseconds m = std::chrono::duration_cast<microseconds>(d);
-	return d.count();
+	return t1 - tZero;
 }
 
 void pureCalculationSingle(int scale){
@@ -45,7 +45,7 @@ int main(int argc, char **argv) {
 		//theory: maximize sceduled time-working????
 		//std::this_thread::sleep_for(microseconds(100));
 		
-		auto t0 = Clock::now();
+		auto t0 = system_clock::time_point(milliseconds(Clock::now()));
 		
 		
 		processing_interrupted = false;
