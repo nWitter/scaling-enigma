9eb0f8d8db63c71718efed02bab0f631619211e3
ga45yax <ga45yax@mytum.de>
2020-01-20 23:32:47 +0100

    ─

--- simple_loop.cpp
+++ simple_loop.cpp
@@ -33,6 +33,6 @@ void interferenceLoop(float timeFraction, float stepLength, int functionType, in
 	nanosec activeTime = nanosec(l);
 
 	int tmp = interference_function(functionType, calcScale, t0, activeTime);
-	
-	printf("\t activeTime %d\t rate %f\t cnt %d\t\n", timeInterv(t0).count(), 1.0 * timeInterv(t0).count() / activeTime.count(), tmp);
+	float r = timeInterv(t0).count() / activeTime.count();
+	printf("\t activeTime %d\t rate %f\t cnt %d\t\n", timeInterv(t0).count(), r, tmp);
 }
