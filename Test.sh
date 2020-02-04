a439a70ce366553357ea52716b50777324d2d176
ga45yax <ga45yax@mytum.de>
2020-02-04 02:11:14 +0100

    c

--- Test.sh
+++ Test.sh
@@ -7,7 +7,7 @@ chmod +x enigma.sh
 
 
 	sbatch --nodes=2 enigma.sh 1000 output/0.out -1 1
-	sbatch --nodes=2 enigma.sh 1000 output/1.out 0 1
+	sbatch --nodes=2 enigma.sh 1000 output/1.out 0.5 1
 	sbatch --nodes=2 enigma.sh 1000 output/2.out 1 1
 	#sbatch --nodes=2 enigma.sh 1200 output/3.out 1 1
 	#sbatch --nodes=2 enigma.sh 800 output/0.out 2 1
