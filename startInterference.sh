2657c434abbb84e07127f45aae58fa670a41e84e
ga45yax <ga45yax@mytum.de>
2020-02-20 16:32:51 +0100

    c

--- startInterference.sh
+++ startInterference.sh
@@ -6,11 +6,11 @@ MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
 ./MPI_Manager $@ &
 
 # launch program
-if [$SLURM_NTASKS == 2]
+if["$SLURM_NTASKS" -eq "2"]
 then
 	$MATRIX_PATH $1 100 100 > $2 &
 fi
-if [$SLURM_NTASKS == 4]
+if["$SLURM_NTASKS" -eq "4"]
 then
 	$MATRIX_PATH $1 100 100 100 100 > $2 &
 fi
