332f3f6078073fc10c1197a462c7fab7caec3411
ga45yax <ga45yax@mytum.de>
2020-03-10 22:54:09 +0100

    u

--- Test.sh
+++ Test.sh
@@ -6,17 +6,18 @@ mkdir -p output
 chmod +x enigma.sh
 
 
-	sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/rr.out -a 1 -i 1 -rr --duration 30
-	sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/4.out -a 4 -i 1 -rr --duration 30
-	sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/0.out -a 1 -i 0 -rr --duration 30
 
-
-#sbatch --ntasks=2 --cpus-per-task=14 --time=00:03:00 ./enigma.sh 500 output/rr.out -a 1 -i 1 -rr --duration 40
-
-#echo "starting n=1 t=7"
-#sbatch --ntasks=1 --cpus-per-task=7 --time=5 enigma.sh 700 n1t7e1.out 1
-
-#echo "starting n=1 t=14"
-#sbatch --ntasks=1 --cpus-per-task=14 --time=5 enigma.sh 700 n1t14e1.out 1
+	#sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/rr.out -a 1 -i 1 -rr --duration 30
+	
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+	sbatch --ntasks=$1 --cpus-per-task=$2 --time=00:04:00 ./enigma.sh 1000 output/$1-$2-$3-$4-$i.out -a $3 -i $4 -rr
+	sleep 10
+done	
+	
+	
+	
+	#sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/4.out -a 4 -i 1 -rr --duration 30
+	#sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/0.out -a 1 -i 0 -rr --duration 30
 
 exit 0
--- aftertest.sh
+++ aftertest.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-for filename in $(dirname $(realpath $0))/output/*; do
-	echo $file
-	input="$file"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for filename in $DIR/output/*; do
+	echo "$filename" >> testout.txt
+	input="$filename"
 	while IFS= read -r line; do
 		echo "$line"
+		if [[ $line == "#R0 Computations with chameleon*"]];
+		then
+			echo "$line" >> testout.txt
+		fi
+		if [[ $line == "#R0: Computations with normal tasking*"]];
+		then
+			echo "$line" >> testout.txt
+		fi
+
+
+"$line"
 	done < "$input"
 done
+
 
 
 
