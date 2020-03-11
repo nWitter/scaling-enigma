470728578bdc68e841c6b670ebfc5b17f35db1fb
ga45yax <ga45yax@mytum.de>
2020-03-11 01:34:33 +0100

    j

--- startInterference.sh
+++ startInterference.sh
@@ -3,13 +3,16 @@
 #
 MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
 
+echo $@
 echo "starting"
 ./MPI_Manager $@ &
 sleep 2
+
 for i in 1 2 3 4 5
 do
 	echo 1
-	$MATRIX_PATH $1 $2 > $3$i.out &
+	$MATRIX_PATH $1 $2 > $3$i.out
+	echo -done
 	sleep 100
 done
 
