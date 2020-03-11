e0db80f2be0cd4862a3e550f1a60ca3b67dbe15e
ga45yax <ga45yax@mytum.de>
2020-03-11 04:38:01 +0100

    5

--- Test.sh
+++ Test.sh
@@ -11,7 +11,7 @@ do
 for i in 1 2 3 4 5
 do
 	echo "task" $a " " $i
-	sbatch --ntasks=8 --cpus-per-task=7 --time=00:02:00 ./enigma.sh $a $i
+	sbatch --ntasks=8 --cpus-per-task=7 --time=00:02:00 ./enigma.sh $a $i &
 	sleep 60
 	sleep 60
 	echo "wait"
