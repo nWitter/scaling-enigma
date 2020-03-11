6a518ab2f803e0b8c306cdfbb00a7103ca2b54a9
ga45yax <ga45yax@mytum.de>
2020-03-11 01:18:45 +0100

    5

--- Test.sh
+++ Test.sh
@@ -11,7 +11,8 @@ chmod +x enigma.sh
 	
 sbatch --ntasks=$1 --cpus-per-task=$2 --time=00:03:00 ./enigma.sh
 	
-	#sbatch --ntasks=56 --cpus-per-task=7 --time=00:10:00 ./enigma.sh
+	#sbatch --ntasks=48 --cpus-per-task=7 --time=00:10:00 ./enigma.sh
+	#sbatch --nodes=12 --tasks-per-node=4 --time=00:10:00 ./enigma.sh
 
 	
 	#sbatch --ntasks=4 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 1000 output/4.out -a 4 -i 1 -rr --duration 30
