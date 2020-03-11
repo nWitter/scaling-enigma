efe7011dbff90d3eb2563d58e4d54495da61bdba
ga45yax <ga45yax@mytum.de>
2020-03-11 16:44:23 +0100

    @

--- Test.sh
+++ Test.sh
@@ -5,13 +5,25 @@
 mkdir -p output
 chmod +x enigma.sh
 
-
+for a in 1 2 3
+do
 for i in 1 2 3 4 5
 do
 	echo "task" $1 " " $i
-	sbatch --ntasks=8 --cpus-per-task=7 --time=00:03:00 ./enigma.sh $1 $i &
+	sbatch --ntasks=8 --cpus-per-task=7 --time=00:03:00 ./enigma.sh 5 $i 8 $a &
 	#sbatch --ntasks=8 --cpus-per-task=7 --time=00:03:00 ./enigma.sh 8 3 &
-	sleep 270
+	sleep 300
+done
+done
+
+for a in 1 2 3
+do
+for i in 1 2 3 4 5
+do
+	echo "seccond task" $1 " " $i
+	sbatch --ntasks=2 --cpus-per-task=28 --time=00:03:00 ./enigma.sh 5 $i 2 $a &
+	sleep 300
+done
 done
 
 
--- aftertest.sh
+++ aftertest.sh
@@ -3,20 +3,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 outfile="testout.txt"
 for filename in $DIR/output/*
 do
-	echo "$filename" >> $outfile
-	input="$filename"
+	echo $filename >> $outfile
+	input=$filename
 	while IFS= read -r line
 	do
-		echo "$line"
 		if [[ $line == "#R0 Computations with chameleon*" ]]; then
 			echo "$line" >> $outfile
 		fi
 		if [[ $line == "#R0: Computations with normal tasking*" ]]; then
 			echo "$line" >> $outfile
 		fi
-
-
-"$line"
 	done < "$input"
 done
 
