0434db08339ab07b329fd6cef968de3725256a5d
ga45yax <ga45yax@mytum.de>
2020-03-11 01:15:21 +0100

    5

--- startInterference.sh
+++ startInterference.sh
@@ -4,25 +4,13 @@
 MATRIX_PATH=../chameleon-apps/applications/matrix_example/main
 
 
-TASKDIST="100"
-if [ "$SLURM_NTASKS" == "2" ]; then
-	TASKDIST="100 100"
-fi
-if [ "$SLURM_NTASKS" == "4" ]; then
-	TASKDIST="100 100 100 100"
-fi
-if [ "$SLURM_NTASKS" == "8" ]; then
-	TASKDIST="100 100 100 100 100 100 100 100"
-fi
-
-echo $TASKDIST
 
 ./MPI_Manager $@ &
 sleep 2
-for i in 1
+for i in 1 2 3 4 5
 do
-	$MATRIX_PATH $1 $TASKDIST > $2$i.out &
-	sleep 60
+	$MATRIX_PATH $1 $2 > $2$i.out &
+	sleep 100
 done
 
 exit
