4b81e9f4d78b0f3fe6f0522c1b506044296ec38f
ga45yax <ga45yax@mytum.de>
2020-03-11 16:54:12 +0100

    4

--- aftertest.sh
+++ aftertest.sh
@@ -4,16 +4,14 @@ outfile="testout.txt"
 for filename in $DIR/output/*
 do
 	echo $filename >> $outfile
-	input=$filename
-	while IFS= read -r line
-	do
+	while IFS= read -r line; do
 		if [[ $line == "#R0 Computations with chameleon*" ]]; then
 			echo "$line" >> $outfile
 		fi
 		if [[ $line == "#R0: Computations with normal tasking*" ]]; then
 			echo "$line" >> $outfile
 		fi
-	done < "$input"
+	done < $filename
 done
 
 
