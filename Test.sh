5283116e571bf08b95c8e78a4653d1381fbb6619
ga45yax <ga45yax@mytum.de>
2020-03-11 14:42:24 +0100

    4

--- Test.sh
+++ Test.sh
@@ -9,8 +9,8 @@ chmod +x enigma.sh
 for i in 1 2 3 4 5
 do
 	echo "task" $1 " " $i
-	sbatch --ntasks=8 --cpus-per-task=7 --time=00:02:00 ./enigma.sh $1 $i &
-	sleep 250
+	sbatch --ntasks=8 --cpus-per-task=7 --time=00:03:00 ./enigma.sh $1 $i &
+	sleep 270
 done
 
 
