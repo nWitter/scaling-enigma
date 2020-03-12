8d22cc21ac781181049249fdf4f2ab77ae92154c
ga45yax <ga45yax@mytum.de>
2020-03-12 02:13:52 +0100

    d

--- startInterference.sh
+++ startInterference.sh
@@ -8,6 +8,17 @@ echo "starting"
 ./MPI_Manager $@ &
 sleep 2
 
-$MATRIX_PATH $1 100 100 100 100 100 100 100 100 > $2
+TSK="100"
+if[["$1" == 2 ]]; then
+	TSK="100 100"
+fi
+if[["$1" == 4 ]]; then
+	TSK="100 100 100 100"
+fi
+if[["$1" == 8 ]]; then
+	TSK="100 100 100 100 100 100 100 100"
+fi
+
+$MATRIX_PATH $1 $TSK > $2
 
 exit
